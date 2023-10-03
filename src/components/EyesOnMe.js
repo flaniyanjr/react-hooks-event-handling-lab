f43cf6abbd79286a36118f8656b6414fba2bf08f
Felix Laniyan <flaniyanjr@gmail.com>
2023-10-03 17:19:38 -0400

    tests passing

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+    function focus() {
+        console.log('Good!')
+    }
+    return(
+        <button onFocus= {focus} onBlur= {() => console.log('Hey! Eyes on me!')}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,12 @@
 // Code Keypad Component Here
+import React from 'react'
 
 function Keypad (){
+    function password() {
+        console.log('Entering password...')
+    }
     return (
-        <div></div>
+        <input type='password' onChange={password}/> 
     )
 }
 
